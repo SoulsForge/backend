dfd4a68ec586e8379292f646da3a5be18295c2b5
Kazte <39470734+Kazte@users.noreply.github.com>
2024-06-14 20:00:33 -0300

    chore: add routes for authentication and user

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { App } from '@/app';
+import { AuthRoute } from './routes/auth.route';
 import { HealthRoute } from './routes/health.route';
+import { UserRoute } from './routes/user.route';
 
-const app = new App([new HealthRoute()]);
+const app = new App([new HealthRoute(), new UserRoute(), new AuthRoute()]);
 
 app.listen();
