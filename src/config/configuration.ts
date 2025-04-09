97bda27089ac4cc612712efadde6c6a0a307d937
Kazte <castellittofranco1@gmail.com>
2025-04-08 22:40:18 -0300

    feat: add SMTP configuration to environment settings

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,4 +2,10 @@ export default () => ({
   port: process.env.PORT,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresIn: process.env.JWT_EXPIRES_IN,
+  smtpConfig: {
+    smtpDomain: process.env.SMTP_DOMAIN,
+    smtpEndpoint: process.env.SMTP_ENDPOINT,
+    smtpUsername: process.env.SMTP_USERNAME,
+    smtpPassword: process.env.SMTP_PASSWORD,
+  },
 });
